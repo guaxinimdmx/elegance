7d56f2406c70a7d60a50a0f01bfc2ea1b4f5be70
André Ricardo <contatoadmx@gmail.com>
2021-02-19 02:54:18 -0300

    Comando MX createProject

--- class/command/MxProjectBase.php
+++ class/command/MxProjectBase.php
@@ -9,15 +9,27 @@ class MxProjectBase extends Cmd
 {
     public function __construct()
     {
-        $this->add('create', 'Cria arquivos básicos para um projeto com elegance');
+        $this->add('create', 'Cria arquivos básicos para um projeto com elegance', true);
     }
 
     #==| Comandos |==#
 
     public function create()
     {
+        Dir::create('./class');
+
+        Dir::create('./helper/function');
+        Dir::create('./helper/constant');
+        Dir::create('./helper/script');
+
+        Dir::create('./library');
+
+        Dir::create('./public/assets');
+
         $to = dirname(__DIR__, 2) . "/library/template/project";
+
         Dir::copy($to, '.');
+
         $this->show('Projeto base criado');
     }
 }
